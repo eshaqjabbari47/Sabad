3585f68ad358aa58faa058fd28cd851329758e46
eshaqjabbari47 <jabbari.eshaq@gmail.com>
2025-02-15 22:53:13 +0330

    add function 'getAllItems' for get all records from table & addannotation to use Room query

--- app/src/main/java/com/eshaq/sabad/model/dao/ItemDao.kt
+++ app/src/main/java/com/eshaq/sabad/model/dao/ItemDao.kt
@@ -1,7 +1,12 @@
 package com.eshaq.sabad.model.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Query
+import com.eshaq.sabad.model.entity.Item
 
 @Dao
 interface ItemDao {
+    @Query("Select * from tbl_Items")
+    fun getAllItems():LiveData<List<Item>>
 }
