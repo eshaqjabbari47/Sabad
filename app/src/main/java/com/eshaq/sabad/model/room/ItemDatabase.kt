35d9c6bb9b35e16a2919dbd2cf41cebd220646be
eshaqjabbari47 <jabbari.eshaq@gmail.com>
2025-02-16 18:49:36 +0330

    feat: create an instance from room db

--- app/src/main/java/com/eshaq/sabad/model/room/ItemDatabase.kt
+++ app/src/main/java/com/eshaq/sabad/model/room/ItemDatabase.kt
@@ -1,6 +1,8 @@
 package com.eshaq.sabad.model.room;
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.eshaq.sabad.model.dao.ItemDao
 import com.eshaq.sabad.model.entity.Item
@@ -9,4 +11,20 @@ import com.eshaq.sabad.model.entity.Item
 abstract class ItemDatabase : RoomDatabase() {
     //Dao interface for the database
     abstract  fun itemsDao(): ItemDao
+
+    @Volatile
+    var INSTANCE : ItemDatabase?=null
+    // Singleton instance of the database
+    fun getDatabaseInstance(context : Context):ItemDatabase {
+        val tempInstance = INSTANCE
+        if(tempInstance!=null) {
+            return tempInstance
+        }
+    // Synchronized block to make sure that
+    // only one instance of the database is created
+    synchronized(this) {
+        val roomDatabaseInstance = Room.databaseBuilder(context,ItemDatabase::class.java,"Items_db").allowMainThreadQueries().build()
+        INSTANCE = roomDatabaseInstance
+        return roomDatabaseInstance             }
+    }
 }
