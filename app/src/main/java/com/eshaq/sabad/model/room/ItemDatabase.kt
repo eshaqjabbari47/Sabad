f1e4917bd494d8f3712d2dd9b977cfe80e7c7764
eshaqjabbari47 <jabbari.eshaq@gmail.com>
2025-02-16 18:07:44 +0330

    feat: add dao to db

--- app/src/main/java/com/eshaq/sabad/model/room/ItemDatabase.kt
+++ app/src/main/java/com/eshaq/sabad/model/room/ItemDatabase.kt
@@ -2,9 +2,11 @@ package com.eshaq.sabad.model.room;
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.eshaq.sabad.model.dao.ItemDao
 import com.eshaq.sabad.model.entity.Item
 
 @Database(entities =[Item::class], version = 1, exportSchema = false)
 abstract class ItemDatabase : RoomDatabase() {
-    
+    //Dao interface for the database
+    abstract  fun itemsDao(): ItemDao
 }
